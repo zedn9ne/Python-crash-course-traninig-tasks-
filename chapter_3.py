2161e33322b5601aa8e27f0bab5e14582119cf0c
Mahdi Zanganeh <mahdizedn9ne@gmail.com>
2024-03-31 00:10:37 +0330

    first comment

---
+++ chapter_3.py
@@ -0,0 +1,15 @@
+# 36
+names = [ 'hesam' , 'saba' , 'hashem','ali','kamran'];
+# print(names[1]);
+# /////
+# for name in names :
+    # print(f'hello my friend {name}');
+# ////////
+
+# favorite_transportation = ['car','airplane', 'metro', 'bicycle'];
+# print(f'Always I would like to travel with Emirates {favorite_transportation[1]}.')
+# print(f'My favorite {favorite_transportation[0]} is mustang 1967 . ')
+# print(f'When i was kid i always wish had a {favorite_transportation[-1]}.')
+
+# ///////////////////////
+
---
+++ hello_world.py
@@ -0,0 +1,33 @@
+name = "mahdi" ;
+last_name = "zanganeh";
+full_name = f"{name} {last_name}";
+message = f"Hello , {full_name.title()}!";
+# print(message);
+
+
+
+# ///////// tabs and new lines
+
+# print("Languages:\n\tPython\n\t\tJavascript\n\t\t\tC\n\t\t\t\tCotlin")
+
+# /////////
+
+person = 'jim carry';
+quotes = 'Behind every great man is a woman rolling her eyes';
+message = f"{person.title()} once said : {quotes}"
+# print(f"{person.title()} once said '{quotes}'.")
+
+# /////// strips
+
+name = "    python    ";
+# print(name.strip())
+
+# //////// remove prefix
+
+website = "https://zedn9neCompany.co";
+# print(website.removesuffix('.co'))
+# print(website.removeprefix("https://"))
+
+# favorite variables page 29
+my_favorite_numbers = "33 , 7 , 27 ";
+print(f"my favorite numbers are: \n{my_favorite_numbers}.")
